5975958765728d0d420f647132068463098da5aa
Rob Crittenden <rcritten@redhat.com>
2016-02-22 20:16:48 +0000

    remove FIXME

--- novajoin/hooks.py
+++ novajoin/hooks.py
@@ -201,7 +201,6 @@ class IPANovaHookBase(object):
         exc = None
         if resp['error'] is None:
             return exc
-        # FIXME: tighten this code up
         errcode = resp['error']['code']
         method = ipareq['method']
         methtype = method.split('_')[0]
