025c81c35b6bdadeffee350f8352950fc2febd64
Rob Crittenden <rcritten@redhat.com>
2016-11-09 20:39:41 +0000

    Become 1.0.7

--- novajoin/notifications.py
+++ novajoin/notifications.py
@@ -126,7 +126,7 @@ class NotificationEndpoint(object):
 
 def main():
     register_keystoneauth_opts(CONF)
-    CONF(sys.argv[1:], project='join', version='1.0.6')
+    CONF(sys.argv[1:], project='join', version='1.0.7')
     logging.setup(CONF, 'join')
 
     transport = oslo_messaging.get_transport(CONF)
