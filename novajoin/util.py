28a010e3ae4e2fb7400ca46f24b76ac0ff1144a7
Rob Crittenden <rcritten@redhat.com>
2016-12-06 20:17:38 +0000

    Make domain an optional configuration option
    This will use the IPA domain by default. If a domain is specified
    then it will be used.
    
    For testing, if there is no config option and IPA is not available
    then the domain 'test' will be mocked.
    
    Change-Id: I21b66c61830c49a79094927f42c22c76ec1b447c

---
+++ novajoin/util.py
@@ -0,0 +1,47 @@
+# Copyright 2016 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Utility functions shared between notify and server"""
+
+from novajoin.errors import ConfigurationError
+from oslo_config import cfg
+from oslo_log import log as logging
+
+try:
+    from ipalib import api
+    ipalib_imported = True
+except ImportError:
+    # ipalib/ipapython are not available in PyPi yet, don't make it
+    # a showstopper for the tests.
+    ipalib_imported = False
+
+CONF = cfg.CONF
+
+LOG = logging.getLogger(__name__)
+
+
+def get_domain():
+    """Retrieve the domain for creating a FQDN.
+
+       By default this will come out of the IPA API but it can be
+       overridden by setting domain in the DEFAULT section of the
+       configuration file.
+    """
+    if CONF.domain:
+        return CONF.domain
+
+    if ipalib_imported:
+        return api.env.domain
+
+    raise ConfigurationError("Unable to get domain")
