b33684b781b895f0fe2e0ac5c38b334df7ba9752
Rob Crittenden <rcritten@redhat.com>
2017-02-14 10:09:10 -0500

    Become 1.0.11
    Change-Id: I8dbebe0fc293867117702c1b16439db9a702dc89

--- novajoin/wsgi.py
+++ novajoin/wsgi.py
@@ -105,7 +105,7 @@ def process_launcher():
 def main():
 
     keystone_client.register_keystoneauth_opts(CONF)
-    CONF(sys.argv[1:], version='1.0.10',
+    CONF(sys.argv[1:], version='1.0.11',
          default_config_files=config.find_config_files())
     log.setup(CONF, 'join')
     launcher = process_launcher()
